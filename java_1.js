65c3143678bcc42e28c5b090c858256c89963a1b
Бушева Диана <113347168+Medic0402Bousheva@users.noreply.github.com>
2023-02-16 15:12:03 +0300

    Add files via upload

---
+++ java_1.js
@@ -0,0 +1,24 @@
+'Привет, Java!'
+var myThing;
+myThing=5;
+myThing="это строка"
+var namberNine= 9;
+var stringNine="9";
+namberNine + namberNine;
+stringNine + stringNine;
+var greeting="Привет,";
+var myName="Ник";
+greeting + myName;
+myName.length;
+var name_orz='заболевшие ОРЗ-';
+var name_vet='заболевшие ветрянкой';
+var num_opz_mon=4;
+var num_opz_tues=0;
+var num_opz_wen=2;
+var num_vet_mon=1;
+var num_vet_tues=6;
+var num_vet_wen= 11;
+num_orz_week= num_opz_mon + num_opz_tues + num_opz_wen;
+name_orz + num_orz_week;
+num_vet_week= num_vet_mon + num_vet_tues + num_vet_wen;
+name_orz + num_orz_week + name_vet + num_vet_week;
